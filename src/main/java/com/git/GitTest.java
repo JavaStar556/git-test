b07d3f4921c01a2a6473d652ca37eddc037d60cf
star7769 <JavaStar556@.163.com>
2022-01-23 17:35:41 +0800

    2022.1.23 hot-fix.3

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -12,7 +12,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!");
     }
 }
