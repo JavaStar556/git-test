5b75282e3aa3d3903dc6b2aa9a92c718cf01e54c
star7769 <JavaStar556@.163.com>
2022-01-23 17:07:18 +0800

    2022.1.23.1

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.git;
+
+/**
+ * className:GitTest
+ * Package:com.git
+ * Description:描述
+ *
+ * @Date:2022/1/23 16:53
+ * @Author:mailbox@123.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
